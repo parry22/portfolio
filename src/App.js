bf1a1b554e86bb7965dd175a16ae99acb49a7c35
parry <pravneet07singh@gmail.com>
2023-08-20 14:12:53 +0530

    hhjg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import Dashboard from './Dashboard';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const Layout=({
+  children
+})=>{
+  return <>
+  <div className="d-flex-container">
+    <div className="d-flex">
+      <Sidebar/>
+    {children}
+    </div>
+  </div>
+  </>
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout><Dashboard /></Layout>,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="bg-gray-900 min-h-screen w-full">
+       {/* <Sidebar /> */}
+        {/* <Dashboard /> */}
+        <RouterProvider router={router} />
+        {/* <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes> */}
+      </div>
+    </Router>
   );
 }
 
 export default App;
+

aa6229267b94c87ef88dded02bca0560ef4b1a9d
parry <pravneet07singh@gmail.com>
2023-09-03 11:29:04 +0530

    dada

--- src/Charts/Barchart.js
+++ src/Charts/Barchart.js
@@ -9,8 +9,6 @@ import {
   Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-// import faker from 'faker';
-import { faker } from '@faker-js/faker';
 
 
 ChartJS.register(
@@ -24,9 +22,23 @@ ChartJS.register(
 
 export const options = {
   responsive: true,
+  scales: {
+    y: {
+      beginAtZero: true,
+      // Customize the y-axis label values using a callback function
+      ticks: {
+        // Use a callback function to format and customize label values
+        callback: function (value, index, values) {
+          // You can apply any custom formatting here
+          return `${value} %`; // Example: Prefixing the value with a dollar sign
+        },
+      },
+    },
+  },
   plugins: {
     legend: {
       position: 'top',
+
     },
   },
 };
@@ -35,21 +47,40 @@ const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
 export const data = {
   labels,
-  datasets: [
-    {
-      label: 'Conversion Rate',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Customer Acquisition',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
+ 
+    datasets: [
+        {
+            "label": "Conversion Rate",
+            "data": [
+                95,
+                35,
+                18,
+                31,
+                5,
+                66,
+                39
+            ],
+            "backgroundColor": "rgba(255, 99, 132, 0.5)"
+        },
+        {
+            "label": "Customer Acquisition",
+            "data": [
+                87,
+                36,
+                49,
+                78,
+                96,
+                20,
+                20
+            ],
+            "backgroundColor": "rgba(53, 162, 235, 0.5)"
+        }
+    ],
+  
 };
 
 const Barchart=()=>{
+  console.log('first bar',data)
   return <Bar options={options} data={data} />;
 }
 export default Barchart
--- src/Charts/Linechart.js
+++ src/Charts/Linechart.js
@@ -23,32 +23,64 @@ ChartJS.register(
   Legend
 );
 
+
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
 export const options = {
   responsive: true,
+  scales: {
+    y: {
+      beginAtZero: true,
+      // Customize the y-axis label values using a callback function
+      ticks: {
+        // Use a callback function to format and customize label values
+        callback: function (value, index, values) {
+          // You can apply any custom formatting here
+          return `${value} %`; // Example: Prefixing the value with a dollar sign
+        },
+      },
+    },
+  },
   plugins: {
     legend: {
       position: 'top',
-    },
-    title: {
-      display: true,
+
     },
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
 export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-  ],
+  
+    "labels": [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July"
+    ],
+    "datasets": [
+        {
+            "label": "Error rate",
+            "data": [
+                12,
+                94,
+                89,
+                50,
+                57,
+                2,
+                28,
+            ],
+            "borderColor": "rgb(255, 99, 132)",
+            "backgroundColor": "rgba(255, 99, 132, 0.5)"
+        }
+    ]
+
 };
 
 export default function Linechart() {
+  console.log('first',data)
   return <Line options={options} data={data} />;
 }

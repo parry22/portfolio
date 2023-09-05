da706ffe1396bb2e088d0a1896f2ed105a1c9e6d
parry <pravneet07singh@gmail.com>
2023-09-05 11:43:13 +0530

    FRFR

--- src/Contact.js
+++ src/Contact.js
@@ -1,128 +1,28 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import React from 'react';
 
-export default function Example() {
+function MyIframe() {
   return (
-    <form>
-      <div className="space-y-12">
-     
-        <div className=" border-white/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-white">Contact</h2>
-          <p className="mt-1 text-sm  leading-6 text-gray-400">Reach out to me for any enquiries.</p>
-          <div className='border-t border-gray-800 my-4'/>
-          <div className="mt-0 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-3">
-              <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-white">
-                First name
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-white">
-                Last name
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-4">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-3">
-              <label htmlFor="country" className="block text-sm font-medium leading-6 text-white">
-                Subject
-              </label>
-              <div className="mt-2">
-                <select
-                  id="country"
-                  name="country"
-                  autoComplete="country-name"
-                  className="block w-full rounded-md border-0 bg-white/5 py-2.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black"
-                >
-                  <option>Coffee chat</option>
-                  <option>Job opportunity</option>
-                  <option>Contract/Freelance project enquiry</option>
-                  <option>Other</option>
-                </select>
-
-                
-     </div>
-
-   </div>
-
- </div>
-</div>
-
-           
-</div>
-<div>
-      <label htmlFor="comment" className="block text-sm font-medium leading-6 text-white">
-        Message
-      </label>
-      <div className="mt-2">
-        <textarea
-          rows={4}
-          name="comment"
-          id="comment"
-          className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6"
-          defaultValue={''}
-        />
-       </div>
-       
-</div>
-
-      
-
-      <div className="mt-6 flex items-center justify-start gap-x-6">
-        <button
-          type="submit"
-          className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-        >
-          Submit
-        </button>
+   <>
+    <div className="px-4 sm:px-0">
+        <h3 className="text-base font-semibold leading-7 text-white">
+          Contact me
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-400">
+          Reach out to me for any queries .
+        </p>
       </div>
-    </form>
-  )
+    <iframe className='mt-10'
+      src="https://tally.so/embed/w5jQdZ?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+      width="100%"
+      height="580"
+      frameBorder="0"
+      marginHeight="0"
+      marginWidth="0"
+      title="Contact form"
+      loading="lazy"
+    ></iframe>
+    </>
+  );
 }
 
+export default MyIframe;
